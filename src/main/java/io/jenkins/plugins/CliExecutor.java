23f329de1c105fcac873fe33f90da01b07fb6615
Bruno Verachten <gounthar@users.noreply.github.com>
2025-01-26 15:56:11 +0100

    Require 2.346.3

--- src/main/java/io/jenkins/plugins/CliExecutor.java
+++ src/main/java/io/jenkins/plugins/CliExecutor.java
@@ -2,7 +2,6 @@ package io.jenkins.plugins;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
